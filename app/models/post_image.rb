235c86106a736a2611a10de71334e9b6a62fbb19
yurizou <daikiiiiiiin777@gmail.com>
2023-05-24 14:56:15 +0000

    [Add]

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -2,6 +2,11 @@ class PostImage < ApplicationRecord
   has_one_attached :image
   belongs_to :user
   has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+
+   def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+   end
 
    def get_image
      if image.attached?

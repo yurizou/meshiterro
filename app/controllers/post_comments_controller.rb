b6588b3bec5c769c16ce16110bb0d3c80890c0db
yurizou <daikiiiiiiin777@gmail.com>
2023-05-24 13:54:21 +0000

    [Add]

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -8,6 +8,12 @@ def create
   redirect_to post_image_path(post_image)
 end
 
+def destroy
+  PostComment.find(params[:id]).destroy
+  redirect_to post_image_path(params[:post_image_id])
+  
+end
+
 
 private
 
